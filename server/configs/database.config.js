08eb2732f8fdb47b43837127737a925de6d9e2df
Ganapati Dattatraya Hegde <ganapati@piktorlabs.com>
2019-06-25 15:31:43 +0530

    First commit

---
+++ server/configs/database.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'mongodb+srv://northstarUser:north123456789@northstar-cluster1-d2xyi.mongodb.net/test?retryWrites=true&w=majority',
+};
---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const dbConfig = require('./configs/database.config.js');
+const mongoose = require('mongoose');
+
+// mongoose.Promise = global.Promise;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+
+app.get('/', (req, res) =>{
+    res.json({message : "Welcom to node server of northStar application"});
+});
+
+app.listen(8080, ()=>{
+    console.log("Listening on port 8080")
+});
