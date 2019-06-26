7d4aef0a57920c621209b3caa7c95ae75e5d0c26
Ganapati Dattatraya Hegde <ganapati@piktorlabs.com>
2019-06-26 13:20:46 +0530

    Moved server to app

--- app/server/server.js
+++ app/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 const dbConfig = require('./configs/database.config.js');
 const mongoose = require('mongoose');
