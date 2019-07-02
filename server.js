c487bd2d47bec286aef759291b14db0e09de1e3b
Ganapati Dattatraya Hegde <ganapati@piktorlabs.com>
2019-07-02 11:54:38 +0530

    stag

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use('/api', route);
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 
