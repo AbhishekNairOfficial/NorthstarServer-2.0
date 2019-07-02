3929cfa9bbbb1b24fbade4d87e55a439e83bb533
Ganapati Dattatraya Hegde <ganapati@piktorlabs.com>
2019-07-02 12:27:39 +0530

    dfind srvr

--- server.js
+++ server.js
@@ -40,39 +40,39 @@ app.use('/api', route);
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-app.listen(server_port, server_host, function() {
+ var server = app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 
 
 
-// const io=socket(server);
+const io=socket(server);
 
-// io.on('connection',(socket)=>{
-//     console.log(chalk.bgBlue("client connected!!",socket.id))
+io.on('connection',(socket)=>{
+    console.log(chalk.bgBlue("client connected!!",socket.id))
     
-//     socket.on('user-join',()=>{
-//         Chats.find().then(response=>{
-//             socket.emit('user-history-chat',response);
-//         }).catch(err=>{
-//             socket.emit('error');
-//         })
+    socket.on('user-join',()=>{
+        Chats.find().then(response=>{
+            socket.emit('user-history-chat',response);
+        }).catch(err=>{
+            socket.emit('error');
+        })
         
-//     })
+    })
 
-//     socket.on('chat-client',(data)=>{
-//         let chatEntery=new Chats(data)
-//         chatEntery.save()
-//         .then(response=>{
-//             io.sockets.emit('chat-server',data);
-//         }).catch(err=>{
-//             socket.emit('error');
-//         })
+    socket.on('chat-client',(data)=>{
+        let chatEntery=new Chats(data)
+        chatEntery.save()
+        .then(response=>{
+            io.sockets.emit('chat-server',data);
+        }).catch(err=>{
+            socket.emit('error');
+        })
        
-//     })
+    })
 
-//     socket.on('typing-client',(data)=>{
-//         socket.broadcast.emit('typing-server',data);
-//     })
+    socket.on('typing-client',(data)=>{
+        socket.broadcast.emit('typing-server',data);
+    })
    
-// })
+})
