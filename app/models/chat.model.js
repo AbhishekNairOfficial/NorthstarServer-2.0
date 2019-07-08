d2a3a31724fc25bbf59bf49a11f92a00e2352516
Suprita S T <suprita@moonraft.com>
2019-07-08 17:56:00 +0530

    no message

--- app/models/chat.model.js
+++ app/models/chat.model.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 let ChatSchema = new Schema({
     userId : {type : String, require : true},
     message:{type:String,require:true},
-    mediaType: {type: String, enum: ['camera', 'file']}
+    mediaType: {type: String, enum: ['camera', 'file']},
+    mediaName: { type: String}
 });
 
 module.exports = mongoose.model('Chats', ChatSchema);
